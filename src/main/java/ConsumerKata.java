a87d0150531336c69865ec1ba266805b02e3d686
VMurtazin <obdorsk@gmail.com>
2023-04-18 17:29:54 +0500

    finalOk

--- src/main/java/ConsumerKata.java
+++ src/main/java/ConsumerKata.java
@@ -2,7 +2,6 @@
 //Для работы предоставляется API по URL - http://94.198.50.185:7081/api/users
 //
 //        Ваша задача: Последовательно выполнить следующие операции и получить код для проверки на платформе:
-//
 //        Получить список всех пользователей
 //        Когда вы получите ответ на свой первый запрос, вы должны сохранить свой session id, который получен через cookie. Вы получите его в заголовке ответа set-cookie. Поскольку все действия происходят в рамках одной сессии, все дальнейшие запросы должны использовать полученный session id ( необходимо использовать заголовок в последующих запросах )
 //        Сохранить пользователя с id = 3, name = James, lastName = Brown, age = на ваш выбор. В случае успеха вы получите первую часть кода.
@@ -10,9 +9,7 @@
 //        Удалить пользователя с id = 3. В случае успеха вы получите последнюю часть кода.
 //        В результате выполненных операций вы должны получить итоговый код, сконкатенировав все его части. Количество символов в коде = 18.
 //
-//
-//        Необходимые данные:
-//
+
 //        Модель пользователя:
 //
 //public class User {
@@ -22,14 +19,15 @@
 //    private Byte age;
 //       ...
 //}
-//
-//
+
 //    Список URL для операций и типы запросов:
 //
 //        Получение всех пользователей - …/api/users ( GET )
 //        Добавление пользователя - …/api/users ( POST )
 //        Изменение пользователя - …/api/users ( PUT )
 //        Удаление пользователя - …/api/users /{id} ( DELETE )
+
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -47,60 +45,31 @@ public class ConsumerKata {
         String url="http://94.198.50.185:7081/api/users";
 
         ResponseEntity<String> response = restTemplate.getForEntity(url,String.class);
-
         String responseHeader = response.getHeaders().getFirst("set-cookie");
-        String body = response.getBody();
+
+        System.out.println(response.getBody());
         String sessionID=responseHeader.substring(0,43);
 
         System.out.println(sessionID);
-//        System.out.println(responseHeader);
-        System.out.println(body);
 
         HttpHeaders headers=new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.add("Cookie",sessionID);
 
-
-        Map<String, String> jsonData = new HashMap<>();
-        jsonData.put("id", "3");
-        jsonData.put("name", "James");
-        jsonData.put("lastName", "Brown");
-        jsonData.put("age", "33");
-
-
-        HttpEntity<Map<String, String>> entity2 = new HttpEntity<>(jsonData, headers);
-        ResponseEntity<String> response2 = restTemplate.postForEntity(url,entity2, String.class);
-        String body2 = response2.getBody();
-        System.out.println(body2);
-
-
-
+        User addedUser=new User(3L,"James","Brown", (byte) 33);
         User PatchUser=new User(3L,"Thomas","Shelby", (byte) 35);
 
 
-        HttpEntity<User> entity3 = new HttpEntity<>(PatchUser, headers);
+        HttpEntity<User> entity2 = new HttpEntity<>(addedUser, headers);
+        String body2 = restTemplate.exchange(url,HttpMethod.POST,entity2, String.class).getBody();
 
-        System.out.println(restTemplate.exchange(url, HttpMethod.PUT, entity3, String.class));
+        HttpEntity<User> entity3 = new HttpEntity<>(PatchUser, headers);
+        String body3=restTemplate.exchange(url, HttpMethod.PUT, entity3, String.class).getBody();
 
         HttpEntity<User> entity4 = new HttpEntity<>(null, headers);
+        String body4=restTemplate.exchange(url+"/3",HttpMethod.DELETE,entity4,String.class).getBody();
 
-        System.out.println(restTemplate.exchange(url+"/3",HttpMethod.DELETE,entity4,String.class));
+        System.out.println(body2+body3+body4);
 
-
-
-
-
-
-
-
-
-
-
-
-//        HttpHeaders headers=new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-
-//        String result=restTemplate.getForObject(url,String.class);
-//        System.out.println(result);
     }
 }
